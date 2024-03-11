40d5f542cec2798d9b67113fb0b66fe4bd6e1a00
yejui626 <yjyejui626@gmail.com>
2024-03-12 01:17:23 +0800

    Updated full version of document chatting with langchain

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,102 +1,21 @@
 import streamlit as st
-from streamlit_chatbox import *
-import time
 import os
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores.chroma import Chroma
-from langchain_community.embeddings.azure_openai import AzureOpenAIEmbeddings
-from utils import init_session,_combine_documents
-from operator import itemgetter
-from langchain.memory import ConversationBufferMemory
-from langchain_core.runnables import RunnableLambda, RunnablePassthrough
-from langchain_openai import AzureChatOpenAI
-from langchain.prompts.prompt import PromptTemplate
-from langchain_core.messages import get_buffer_string
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import AzureOpenAIEmbeddings
+from chat import Chat
+
 
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_PROJECT"] = f"RAG with Doc"
 os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
-os.environ["LANGCHAIN_API_KEY"] = "ls__c584ae4bd8e143bcb41d7ac36eb54752"
-
-memory = ConversationBufferMemory(
-    return_messages=True, output_key="answer", input_key="question"
-)
-loaded_memory = RunnablePassthrough.assign(
-    chat_history=RunnableLambda(memory.load_memory_variables) | itemgetter("history"),
-)
-
-llm = AzureChatOpenAI(
-    deployment_name = "gpt-35-turbo-16k",
-    azure_endpoint=st.secrets['OPENAI_API_ENDPOINT'],
-    openai_api_type="azure",
-    openai_api_version = "2023-07-01-preview"
-)
+os.environ["LANGCHAIN_API_KEY"] = st.secrets['LANGCHAIN_API_KEY']
 
 
 st.title("title bar : Chat with docs, powered by :) 💬🦙")
 st.info("Info bar", icon="📃")
          
-if "messages" not in st.session_state.keys(): # Initialize the chat messages history
-    st.session_state.messages = [
-        {"role": "assistant", "content": "Ask me a question about this folder!"}
-    ]
-
-class Index:
-    def __init__(self, db):
-        self.db = db
-
-    def chat(self, prompt):
-        with st.spinner(text="Analyzing the documents... This should take 1-2 minutes."):
-            retriever = self.db.as_retriever()
-            _template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
-
-            Chat History:
-            {chat_history}
-            Follow Up Input: {question}
-            Standalone question:"""
-            CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
-
-            template = """Answer the question based only on the following context:
-            {context}
-
-            Question: {question}
-            """
-            ANSWER_PROMPT = ChatPromptTemplate.from_template(template)
-
-            standalone_question = {
-                "standalone_question": {
-                    "question": lambda x: x["question"],
-                    "chat_history": lambda x: get_buffer_string(x["chat_history"]),
-                }
-                | CONDENSE_QUESTION_PROMPT
-                | llm
-                | StrOutputParser(),
-            }
-
-            # Now we retrieve the documents
-            retrieved_documents = {
-                "docs": itemgetter("standalone_question") | retriever,
-                "question": lambda x: x["standalone_question"],
-            }
-            # Now we construct the inputs for the final prompt
-            final_inputs = {
-                "context": lambda x: _combine_documents(x["docs"]),
-                "question": itemgetter("question"),
-            }
-            # And finally, we do the part that returns the answers
-            answer = {
-                "answer": final_inputs | ANSWER_PROMPT | llm,
-                "docs": itemgetter("docs"),
-            }
-            # And now we put it all together!
-            final_chain = loaded_memory | standalone_question | retrieved_documents | answer
-            inputs = {"question": prompt}
-            result = final_chain.invoke(inputs)
-            memory.save_context(inputs, {"answer": result["answer"].content})
-            return result["answer"].content
 
 def change_folder(folder):
     with st.spinner("Thinking..."):
@@ -107,19 +26,18 @@ def change_folder(folder):
                     embedding_function=AzureOpenAIEmbeddings(
                                         deployment = "ada002",
                                         model="text-embedding-ada-002",
-                                        openai_api_base=st.secrets['OPENAI_API_ENDPOINT'],
-                                        openai_api_type="azure",
+                                        azure_endpoint=st.secrets['OPENAI_API_ENDPOINT'],
                                         openai_api_version = "2023-07-01-preview"
                                         )
                     )
-        index = Index(db)
+        index = Chat(db)
         print(index)
         st.session_state.chat_engine = index
 
 with st.sidebar:
     st.subheader('Document Chatbot!')
     with st.form("change_folder",clear_on_submit=False,border=False):
-        folder = st.text_input('Folder name',placeholder="Enter your desired folder name")
+        folder = st.text_input('Folder name',placeholder="Enter the folder name to chat with.....")
 
         change_folder_button = st.form_submit_button("Change Folder")
         if change_folder_button:
@@ -151,14 +69,13 @@ with st.sidebar:
                 with st.spinner("Thinking..."):
                     # Check if files are uploaded
                     if uploaded_files is not None:
-                        chroma_directory=f"/Users/user/Desktop/rag_with_documents{folder_path}/"
+                        chroma_directory=f"/Users/user/Desktop/rag_with_documents/{folder_path}/"
                         db = Chroma(collection_name=folder_path,
                                     persist_directory=chroma_directory, 
                                     embedding_function=AzureOpenAIEmbeddings(
                                                         deployment = "ada002",
                                                         model="text-embedding-ada-002",
-                                                        openai_api_base=st.secrets['OPENAI_API_ENDPOINT'],
-                                                        openai_api_type="azure",
+                                                        azure_endpoint=st.secrets['OPENAI_API_ENDPOINT'],
                                                         openai_api_version = "2023-07-01-preview"
                                                         )
                                     )
@@ -187,7 +104,14 @@ with st.sidebar:
 
     cols = st.columns(1)
     if cols[0].button('Refresh'):
-        st.rerun()
+        print("session state before clearing",st.session_state)
+        st.session_state.clear()
+        print("session state after clearing",st.session_state)
+
+if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+    st.session_state.messages = [
+        {"role": "assistant", "content": f"Ask me a question about {folder}!"}
+    ]
 
 if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
     default_directory=f"/Users/user/Desktop/rag_with_documents/default"
@@ -195,12 +119,11 @@ if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
                 embedding_function=AzureOpenAIEmbeddings(
                                     deployment = "ada002",
                                     model="text-embedding-ada-002",
-                                    openai_api_base=st.secrets['OPENAI_API_ENDPOINT'],
-                                    openai_api_type="azure",
+                                    azure_endpoint=st.secrets['OPENAI_API_ENDPOINT'],
                                     openai_api_version = "2023-07-01-preview"
                                     )
                 )
-    index = Index(db)
+    index = Chat(db)
     st.session_state.chat_engine = index
 
 if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
@@ -218,9 +141,6 @@ if st.session_state.messages[-1]["role"] != "assistant":
             st.write(response)
             message = {"role": "assistant", "content": response}
             st.session_state.messages.append(message) # Add response to message history
-
-
-
 
 
 
--- utils.py
+++ utils.py
@@ -3,22 +3,6 @@ import time
 from langchain.prompts.prompt import PromptTemplate
 from langchain_core.prompts import format_document
 
-def init_session(_session_key, _chat_name, clear: bool =False):
-    if not chat_inited or clear:
-        st.session_state[_session_key] = {}
-        time.sleep(0.1)
-        reset_history(_chat_name)
-
-def chat_inited(_session_key):
-    return _session_key in st.session_state.keys()
-
-def reset_history(_session_key,_chat_name, name=None):
-    if not chat_inited:
-        st.session_state[_session_key] = {}
-
-    name = name or _chat_name
-    st.session_state[_session_key][name] = []
-
 DEFAULT_DOCUMENT_PROMPT = PromptTemplate.from_template(template="{page_content}")
 
 
